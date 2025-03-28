d6fb426283ffe90b6fe38589ba3173c73f7dee5e
joaomarcosribeiretee <jmribeirete@hotmail.com>
2025-03-28 18:25:20 -0300

    ajustes finais antes do deploy

--- src/components/MobileNotice.jsx
+++ src/components/MobileNotice.jsx
@@ -14,12 +14,6 @@ const MobileNotice = () => {
           <SocialIcons />
         </div>
 
-        <div className="wave-container-mobile">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#ffffff" fillOpacity="0.4" d="M0,192L80,165.3C160,139,320,85,480,80C640,75,800,117,960,154.7C1120,192,1280,224,1360,240L1440,256V320H1360C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320H0V192Z" />
-          </svg>
-        </div>
-
       </div>
     </div>
   );
